8b260c1b861ebe4254d20416c11ad27647f24e28
nakano-kota <naknao.kota.yb@tut.jp>
2023-09-27 16:59:39 +0900

    add  iris_sample.py

---
+++ eyeRobot/iris_sample.py
@@ -0,0 +1,56 @@
+import cv2
+
+fontType = cv2.FONT_HERSHEY_COMPLEX
+
+# Trimming area
+xmin, xmax = 252, 335  #100 , 500
+ymin, ymax = 250, 435  #100 , 300
+w_meter = 100
+h_meter = 100
+
+# Setting of USB camera
+cap = cv2.VideoCapture(1)  # 0+cv2.CAP_DSHOW
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while True :
+    #frame取得
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    fil = cv2.GaussianBlur(frame, (5, 5), 1)
+    gray_img = cv2.cvtColor(fil[ymin:ymax, xmin:xmax], cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_img, 100, 170,)   # Set upper and lower thresholds
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE, offset = (xmin, ymin))
+
+    for i, cnt in enumerate(contours):
+        center, radius = cv2.minEnclosingCircle(cnt)
+        x = int(center[0])
+        y = int(center[1])
+        r = int(radius)
+        image_size = 4 * (r ** 2)
+        trim_gray = cv2.cvtColor(frame[(y-r):(y+r), (x-r):(x+r)], cv2.COLOR_RGB2GRAY)
+        trim_bin = cv2.threshold(trim_gray, 55, 255, cv2.THRESH_BINARY)[1]
+        white = cv2.countNonZero(trim_bin)
+        black = image_size - white
+        white_ratio = (white/image_size) * 100
+        black_ratio = (black/image_size) * 100
+        if white_ratio < 30 and black_ratio > 70:
+            if radius < 20 and radius > 14:
+                    cv2.circle(frame, center, r, (100, 255, 0), 2)
+                    cv2.circle(frame, center, 2, (0, 0, 255), 3)
+                    cx = (w_meter * center[0])/w
+                    cy = (h_meter * center[1])/h
+        break
+
+    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+
+    cv2.imshow('output', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
--- eyeRobot/smallframe.py
+++ eyeRobot/smallframe.py
@@ -9,8 +9,8 @@ fps = int(cap1.get(cv2.CAP_PROP_FPS))
 width = int(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-xmin, xmax = 320, 403  #100 , 500
-ymin, ymax = 250, 435  #100 , 300
+# xmin, xmax = 320, 403  #100 , 500
+# ymin, ymax = 250, 435  #100 , 300
 
 xs, xl = 270, 380
 ys, yl = 110, 200
@@ -23,11 +23,11 @@ while True:
     ret1, frame1 = cap1.read()
     # frame = frame[ymin:ymax, xmin:xmax]
     # frame1 = frame1[ys:yl, xs:xl]
-    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-    cv2.imshow("frame", frame)
+    # cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+    cv2.imshow("frame", frame1)
     # cv2.imshow("save frame", frame1)
 
-    video.write(frame1)
+    # video.write(frame1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
