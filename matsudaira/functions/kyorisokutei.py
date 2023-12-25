e2bee8bc2ff7248ab323144d2711313bf4463fb3
kouki <matsudaira.koki.we@tut.jp>
2023-12-25 15:01:57 +0900

    commit message

--- matsudaira/functions/kyorisokutei.py
+++ matsudaira/functions/kyorisokutei.py
@@ -4,6 +4,8 @@ import numpy as np
 #import openpyxl
 #import pandas as pd
 import xlwings as xw
+import time
+t = time.time()
 
 #excel起動
 #wb = xw.Book()
@@ -12,21 +14,28 @@ import xlwings as xw
 threshold = 100 #二値化に用いる閾値
 
 # Trimming area
-xmin, xmax = 50, 500  #100 , 500 # 640まで
-ymin, ymax = 150, 300  #100 , 300 # 480まで
+xmin, xmax = 200, 640 #100 , 500 # 640まで 290,440
+ymin, ymax = 220, 320  #100 , 300 # 480まで
 
+xrange = xmax - xmin
+yrange = ymax - ymin
 # Setting of USB camera
 cap = cv2.VideoCapture(0)  # 0+cv2.CAP_DSHOW
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+#fps = int(cap.get(cv2.CAP_PROP_FPS))
+fps = 15
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-video = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\output.mp4', fourcc, fps, (450, 150))
-video_bin = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\output_bin.mp4', fourcc, fps, (450, 150))
-video_img_result = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\img_result.mp4', fourcc, fps, (450, 150))
-video_dst = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dst.mp4', fourcc, fps, (450, 150))
-video_dst3 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dst3.mp4', fourcc, fps, (450, 150))
-video_erosion1 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\erosion1.mp4', fourcc, fps, (450, 150))
+video = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\output.mp4', fourcc, fps, (xrange, yrange))
+#video2 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\output2.mp4', fourcc, fps, (xrange, yrange))
+#video_bin = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\output_bin.mp4', fourcc, fps, (xrange, yrange))
+video_img_result = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\img_result.mp4', fourcc, fps, (xrange, yrange))
+#video_dst = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dst.mp4', fourcc, fps, (xrange, yrange))
+#video_dst2 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dst2.mp4', fourcc, fps, (xrange, yrange))
+#video_dst3 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dst3.mp4', fourcc, fps, (xrange, yrange))
+#video_erosion1 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\erosion1.mp4', fourcc, fps, (xrange, yrange))
+#video_dilation_line2_reverse = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\dilation_line2_reverse.mp4', fourcc, fps, (xrange, yrange))
+#video_bitwise_and = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\VSCode_video\bitwise_and.mp4', fourcc, fps, (xrange, yrange))
 
 #　縦線を消すカーネル
 kernel = np.array([[0, 0, -1],  
@@ -35,7 +44,7 @@ kernel = np.array([[0, 0, -1],
 
 #　横線を消すカーネル
 kernel2 = np.array([[0, 0, 0], 
-                    [0, -1, 1], 
+                    [1, -1, 0], 
                     [0, 0, 0],])
 
 
@@ -43,63 +52,86 @@ kernel3 = np.ones((3, 3), np.uint8)
 #kernel3 = np.ones((5,5),np.uint8)
 
 lines = None # linesの変数を定義
+erosion1_trimming = None
 img_result = None
+#dst3 = None
+
+total_count = 0
+detection_count = 0
+detection_rate = 0
 
 while True :
-
+    
     #frame取得
     ret, frame = cap.read()
+    ret2, frame2 = cap.read()
+    
     if not ret:
         break
-    
+    if not ret2:
+        break
+
     fil = cv2.GaussianBlur(frame, (5, 5), 1)
     gray_img = cv2.cvtColor(fil[ymin:ymax, xmin:xmax], cv2.COLOR_BGR2GRAY)
-    ret, img_thresh = cv2.threshold(gray_img, 80, 255, cv2.THRESH_BINARY) #　2値化
+    ret, img_thresh = cv2.threshold(gray_img, 70, 255, cv2.THRESH_BINARY) #　2値化
     img_reverse = cv2.bitwise_not(img_thresh) #　白黒反転
-    #edges = cv2.Canny(img_thresh, 100, 170)   # Set upper and lower thresholds 100, 170
     
     mask = np.zeros(img_thresh.shape, np.uint8) #黒い画面を作る
-    # mask2 = np.zeros(img_thresh.shape, np.uint8) #黒い画面を作る
+    mask_white = np.ones(img_thresh.shape, np.uint8)*255
 
     #　白色のものを輪郭抽出
     contours, hierarchy = cv2.findContours(img_reverse, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
     for i,cnt in enumerate(contours):
-        if cv2.contourArea(cnt) < 1500: # 1000~1500くらい
+        if cv2.contourArea(cnt) < 3000: # 1000~1500くらい
             img_result = cv2.drawContours(mask, [cnt], -1, 255, thickness=-1) # 面積の小さいものを表示させる
-    
 
     dst = cv2.filter2D(img_result, -1, kernel) #　カーネルで横線の輪郭抽出(まぶた)
     dst2 = cv2.filter2D(img_result, -1, kernel2) #　カーネルで縦線の輪郭抽出(黒目)
-    #dilation_line2 = cv2.dilate(dst, kernel = kernel3, iterations = 1) #　線を太くする。
+
+    cv2.imshow("dst2", dst2)
+   
 
     #　上で抽出した横線を縦線抽出の映像にかぶせて
     contours2, hierarchy2 = cv2.findContours(dst, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for i2,cnt2 in enumerate(contours2):
         dst3 = cv2.drawContours(dst2, [cnt2], -1, 0, thickness=-1) # 対象の塊を黒く塗りつぶす
 
-    dilation_line = cv2.dilate(dst3, kernel = kernel3, iterations = 1) #　線を太くする。dst2もdst3も一緒の映像が入ってる
+    dilation_line2 = cv2.dilate(dst, kernel = kernel3, iterations = 1)
+    dilation_line2_reverse = cv2.bitwise_not(dilation_line2)
+    bitwise_and = cv2.bitwise_and(dst2, dilation_line2_reverse)
+
+    cv2.imshow("dilation_line2_reverse", dilation_line2_reverse)
+    cv2.imshow("bitwise_and", bitwise_and)
+    dilation_line = cv2.dilate(bitwise_and, kernel = kernel3, iterations = 1) #　線を太くする。dst2もdst3も一緒の映像が入ってる
     erosion1 = cv2.erode(dilation_line,kernel3,iterations = 1) #　線を細くする。
     #img_mask = cv2.medianBlur(erosion1, ksize = 3)
     
-    
     lines2 = cv2.HoughLinesP(dst, rho=1, theta=np.pi/360, threshold=10, minLineLength=30, maxLineGap=5) #　まぶた検出
-    
     
     if lines2 is not None: #　まぶた線　eyelid Eye Detection Area
         for line2 in lines2:
             x3, y3, x4, y4 = line2[0]
             xmax_c2 = x4
             ymin_c2 = y3
-            EyeDA_ymin = y4-10  #Eye Detection Area
-            EyeDA_ymax = y4+60
-            EyeDA_xmin = x4-30
-            EyeDA_xmax = x4+20
+            EyeDA_ymin = y4-30  #Eye Detection Area xの差50 yの差70
+            EyeDA_ymax = y4+40
+            EyeDA_xmin = x4-60
+            EyeDA_xmax = x4-10
             #cv2.circle(frame, (x4+xmin, y4+ymin), 15, (255, 255, 255), thickness=-1)
-            frame = cv2.line(frame, (x3+xmin, y3+ymin), (x4+xmin, y4+ymin), (255, 0, 255), 3) #　まぶたの線
-            cv2.rectangle(frame, ( EyeDA_xmin+xmin, EyeDA_ymin+ymin), (EyeDA_xmax+xmin, EyeDA_ymax+ymin), (0, 255, 255), 2) #　瞳検出場所
+            katamuki = abs((y4-y3)/(x3-x4))
+            if 0 < katamuki < 2:
+                frame2 = cv2.line(frame2, (x3+xmin, y3+ymin), (x4+xmin, y4+ymin), (255, 0, 255), 2) #　まぶたの線
+                cv2.rectangle(frame2, ( EyeDA_xmin+xmin, EyeDA_ymin+ymin), (EyeDA_xmax+xmin, EyeDA_ymax+ymin), (0, 255, 255), 2) #　瞳検出場所
+                erosion1_trimming = erosion1[EyeDA_ymin:EyeDA_ymax, EyeDA_xmin:EyeDA_xmax] #　瞳検出エリアトリミング
+                lines = cv2.HoughLinesP(erosion1_trimming, rho=1, theta=np.pi/360, threshold=10, minLineLength=10, maxLineGap=5) #　黒目線検出
+
+    else: #まぶたが検出されない限り、検出できたときの検出エリアで虹彩を検出する
+        if erosion1_trimming is not None:
+
             erosion1_trimming = erosion1[EyeDA_ymin:EyeDA_ymax, EyeDA_xmin:EyeDA_xmax] #　瞳検出エリアトリミング
             lines = cv2.HoughLinesP(erosion1_trimming, rho=1, theta=np.pi/360, threshold=10, minLineLength=10, maxLineGap=5) #　黒目線検出
-            
+        else:
+            continue   
 
     if lines is not None:
         for line in lines:
@@ -125,53 +157,64 @@ while True :
             black_ratio = (black/image_size) * 100
             
 
-            if (differencex <= 5) and (differencey >= 10): #and (y1 > y2)
-                if white_ratio < 80 and white_ratio > 65:
-                    #frame = cv2.line(frame, (x1+xmin, y1+ymin), (x2+xmin, y2+ymin), (0, 0, 255), 3)
-                    #cv2.rectangle(frame, ( xmin_c+xmin, ymin_c+ymin), (xmax_c+xmin, ymax_c+ymin), (0, 255, 0), 2)
+            if (differencex <= 10) and (differencey >= 20): #and (y1 > y2)
+                if white_ratio < 75 and white_ratio > 55:
 
                     frame = cv2.line(frame, (xmin+EyeDA_xmin+x1, ymin+EyeDA_ymin+y1), (xmin+EyeDA_xmin+x2, ymin+EyeDA_ymin+y2), (0, 0, 255), 3) #　瞳の線
                     #cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
                     cv2.rectangle(frame, ( xmin+EyeDA_xmin+xmin_c, ymin+EyeDA_ymin+ymin_c), (xmin+EyeDA_xmin+xmax_c, ymin+EyeDA_ymin+ymax_c), (0, 255, 0), 2)
-
+                    detection_count += 1
                     #瞳の検出座標(x)をリストに追加していく
                     #mylist.append(xmiddle)
 
                     #print(white_ratio)  
-                    #print(xmiddle)
+                    print(xmiddle+EyeDA_xmin+xmin)
                     break
 
     #　トリミングエリア指定
-    frame_trimming = frame[ymin:ymax, xmin:xmax]   
+    frame_trimming = frame[ymin:ymax, xmin:xmax]
+    frame_trimming2 = frame2[ymin:ymax, xmin:xmax]   
     #　検出映像表示
     cv2.imshow('output', frame_trimming)
     #　検出範囲表示（青枠）    
-    frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+    #frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
 
     cv2.imshow("img_result", img_result)
     cv2.imshow("dst", dst)
-    cv2.imshow("dst3", dst3)
+    #cv2.imshow("dst3", dst3)
     cv2.imshow("erosion1", erosion1)
     cv2.imshow("dilation_line", dilation_line)
     cv2.imshow("frame", frame)
-    
+    cv2.imshow("output2", frame_trimming2)
     
 
     #　映像保存
     video.write(frame_trimming)
-    video_bin.write(cv2.cvtColor(img_thresh, cv2.COLOR_GRAY2BGR)) #2値化映像保存
+    #video2.write(frame_trimming2)
+    #video_bin.write(cv2.cvtColor(img_thresh, cv2.COLOR_GRAY2BGR)) #2値化映像保存
     video_img_result.write(cv2.cvtColor(img_result, cv2.COLOR_GRAY2BGR))
-    video_dst.write(cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR))
-    video_dst3.write(cv2.cvtColor(dst3, cv2.COLOR_GRAY2BGR))
-    video_erosion1.write(cv2.cvtColor(erosion1, cv2.COLOR_GRAY2BGR))
+    #video_dilation_line2_reverse.write(cv2.cvtColor(dilation_line2_reverse, cv2.COLOR_GRAY2BGR))
+    #video_dst.write(cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR))
+    #video_dst3.write(cv2.cvtColor(dst3, cv2.COLOR_GRAY2BGR))
+    #video_erosion1.write(cv2.cvtColor(erosion1, cv2.COLOR_GRAY2BGR))
+    #video_bitwise_and.write(cv2.cvtColor(bitwise_and, cv2.COLOR_GRAY2BGR))
+    #video_dst2.write(cv2.cvtColor(dst2, cv2.COLOR_GRAY2BGR))
+    total_count += 1
     
-    
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    c = time.time()
+    if c - t >= 10 or cv2.waitKey(1) & 0xFF == ord('q'):  
         #xw.Range("B2", transpose=True).value = mylist
+        #detection_rate = (detection_count/total_count)*100
+        #xw.Range("C2", transpose=True).value = detection_rate
+        #xw.Range("D2", transpose=True).value = detection_count
+        #xw.Range("E2", transpose=True).value = total_count
         break
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #xw.Range("B2", transpose=True).value = mylist
+        
+        #break
 
 cap.release()
 video.release()
-video_bin.release()
+#video_bin.release()
 cv2.destroyAllWindows()
--- matsudaira/functions/kyorisokutei2.py
+++ matsudaira/functions/kyorisokutei2.py
@@ -1,39 +1,97 @@
 import cv2
 import math
 import numpy as np
+import time
+t = time.time()
 
-fontType = cv2.FONT_HERSHEY_COMPLEX
-threshold = 100 #二値化に用いる閾値
+#fontType = cv2.FONT_HERSHEY_COMPLEX
+#threshold = 100 #二値化に用いる閾値
 
 # Trimming area
-xmin, xmax = 252, 600  #100 , 500
-ymin, ymax = 150, 300  #100 , 300
-w_meter = 100
-h_meter = 100
+xmin, xmax = 270, 420  #250 , 400 # 640まで
+ymin, ymax = 220, 320  #200 , 300 # 480まで
+
+xrange = xmax - xmin
+yrange = ymax - ymin
+
+kernel3 = np.ones((3, 3), np.uint8)
 
 # Setting of USB camera
-cap = cv2.VideoCapture(2)  # 0+cv2.CAP_DSHOW
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+cap = cv2.VideoCapture(0)  # 0+cv2.CAP_DSHOW
+#fps = int(cap.get(cv2.CAP_PROP_FPS))
+fps =15
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+video = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\kyorisokutei2\output.mp4', fourcc, fps, (xrange, yrange))
+video2 = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\kyorisokutei2\output2.mp4', fourcc, fps, (xrange, yrange))
+video_bin = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\kyorisokutei2\output_bin.mp4', fourcc, fps, (xrange, yrange))
+video_edges = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\kyorisokutei2\edges_bin.mp4', fourcc, fps, (xrange, yrange))
+video_reverse = cv2.VideoWriter(R'C:\Users\mkouk\Desktop\kyorisokutei2\reverse_bin.mp4', fourcc, fps, (xrange, yrange))
+#　縦線を消すカーネル
+kernel = np.array([[0, 0, -1],  
+                    [0, 0, 1], 
+                    [0, 0, 0],])
+
+edges_trimming = None
+
+ellipseW = None
 
 while True :
     #frame取得
     ret, frame = cap.read()
+    ret2, frame2 = cap.read()
+
     if not ret:
         break
-    
+    if not ret2:
+        break
+
     fil = cv2.GaussianBlur(frame, (5, 5), 1)
     gray_img = cv2.cvtColor(fil[ymin:ymax, xmin:xmax], cv2.COLOR_BGR2GRAY)
-    ret, img_thresh = cv2.threshold(gray_img, 80, 255, cv2.THRESH_BINARY) #オブジェクトimg_blurを閾値threshold = 100で二値化しimg_binaryに代入
+    ret, img_thresh = cv2.threshold(gray_img, 40, 255, cv2.THRESH_BINARY) #オブジェクトimg_blurを閾値threshold = 100で二値化しimg_binaryに代入
     edges = cv2.Canny(img_thresh, 100, 170)   # Set upper and lower thresholds 100, 170
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE, offset = (xmin, ymin))
+    #contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE, offset = (xmin, ymin))
+    img_reverse = cv2.bitwise_not(img_thresh) #　白黒反転
 
+    dst = cv2.filter2D(img_reverse, -1, kernel) #　カーネルで横線の輪郭抽出(まぶた)
+    dilation_line = cv2.dilate(dst, kernel = kernel3, iterations = 1)
+    cv2.imshow('dst', dst)
+
+    lines2 = cv2.HoughLinesP(dilation_line, rho=1, theta=np.pi/360, threshold=10, minLineLength=30, maxLineGap=5) #　まぶた検出
+    
+    
+    if lines2 is not  None: #　まぶた線　eyelid Eye Detection Area
+        for line2 in lines2:
+            x3, y3, x4, y4 = line2[0]
+            xmax_c2 = x4
+            ymin_c2 = y3
+            EyeDA_ymin = y4-30  #Eye Detection Area
+            EyeDA_ymax = y4+40
+            EyeDA_xmin = x4-60
+            EyeDA_xmax = x4-10
+            #cv2.circle(frame, (x4+xmin, y4+ymin), 15, (255, 255, 255), thickness=-1)
+            frame2 = cv2.line(frame2, (x3+xmin, y3+ymin), (x4+xmin, y4+ymin), (255, 0, 255), 2) #　まぶたの線
+            cv2.rectangle(frame2, ( EyeDA_xmin+xmin, EyeDA_ymin+ymin), (EyeDA_xmax+xmin, EyeDA_ymax+ymin), (0, 255, 255), 2) #　瞳検出場所
+            #edges_trimming = edges[EyeDA_ymin:EyeDA_ymax, EyeDA_xmin:EyeDA_xmax] #　瞳検出エリアトリミング
+            edges_trimming = edges[EyeDA_ymin:EyeDA_ymax, EyeDA_xmin:EyeDA_xmax] #　瞳検出エリアトリミング
+
+    else: #まぶたが検出されない限り、検出できたときの検出エリアで虹彩を検出する
+        if edges_trimming is not None:
+
+            #frame2 = cv2.line(frame2, (x3+xmin, y3+ymin), (x4+xmin, y4+ymin), (255, 0, 255), 2) #　まぶたの線
+            #cv2.rectangle(frame2, ( EyeDA_xmin+xmin, EyeDA_ymin+ymin), (EyeDA_xmax+xmin, EyeDA_ymax+ymin), (0, 255, 255), 2) #　瞳検出場所
+            edges_trimming = edges[EyeDA_ymin:EyeDA_ymax, EyeDA_xmin:EyeDA_xmax] #　瞳検出エリアトリミング
+        else:
+            continue
+    
 
     #cv2.imshow("rinkaku", edges)
-    cv2.imshow("2値化画像", img_thresh)
+    cv2.imshow("bin", img_thresh)
     #cv2.drawContours(edge)
     
+    #虹彩測定範囲の中で楕円フィッティングをしてる
+    contours, hierarchy = cv2.findContours(edges_trimming, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE, offset = (EyeDA_xmin + xmin,EyeDA_ymin + ymin))
     for i, cnt in enumerate(contours):
         #print(f"contour: {i}, number of points: {len(cnt)}")
         # 楕円フィッティング
@@ -47,55 +105,69 @@ while True :
             # print(ellipse[0][0])
 
             
-            ellipseW = (ellipse[1][0])
-            ellipseH = (ellipse[1][1])
-            angle = ellipse[2]
-            ellipseArea = ellipseW*ellipseH
+            ellipseW = ellipse[1][0]
+            ellipseH = ellipse[1][1]
+            angle = int(ellipse[2])
+            #ellipseArea = ellipseW*ellipseH
             
             #print(ellipseW, ellipseH)
 
             #print(angle)
             if ellipseW >= 5 and ellipseW <= 20:
-                if ellipseH >= 20 and ellipseH <= 40:
+                if ellipseH >= 10 and ellipseH <= 70:
                     if (angle >= 0 and angle <=10) or (angle >= 350 and angle <=360): 
                 # 楕円描画
-                        cx = (ellipse[0][0])
-                        cy = (ellipse[0][1])
-                        cx = int(cx)
-                        cy = int(cy)
+                        cx = int(ellipse[0][0])
+                        cy = int(ellipse[0][1])
 
-                        
-                         #cv2.putText(resimg, str(i+1), (cx+3,cy+3), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,80,255), 1,cv2.LINE_AA)
+                        #cv2.putText(resimg, str(i+1), (cx+3,cy+3), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,80,255), 1,cv2.LINE_AA)
                         #print(ellipseArea)
                         #cv2.imshow('resimg',resimg)
-                        ymin_c = int(cy - ellipseW)
-                        ymax_c = int(cy + ellipseW)
-                        xmin_c = int(cx - ellipseH)
-                        xmax_c = int(cy + ellipseH)
-                        image_size = 4 * (ellipseW*ellipseH)
+                        xmax_c = cx + 20 #　白黒比計測エリア
+                        xmin_c = cx - 20
+                        ymax_c = cy + 20
+                        ymin_c = cy - 20
+                        image_size = 40*40
+                        
 
                         if image_size == 0:
                             break
         
-                        #trim_bin = img_thresh[ymin_c-ymin:ymax_c-ymin, xmin_c-xmin:xmax_c-xmin]
+                        trim_bin = img_thresh[ymin_c-ymin:ymax_c-ymin, xmin_c-xmin:xmax_c-xmin]
+                        
                         #trim_bin = img_thresh[20:140, 20:340]
                         #cv2.imshow("trim", trim_bin)
                         
-                        #white = cv2.countNonZero(trim_bin)
+                        white = cv2.countNonZero(trim_bin)
                         #black = image_size - white
-                        #white_ratio = (white/image_size) * 100
+                        white_ratio = (white/image_size) * 100
+                        #print(white_ratio)
                         #black_ratio = (black/image_size) * 100
-                        #if white_ratio < 30 and white_ratio > 0:
-                        #    frame = cv2.ellipse(frame,ellipse,(255,0,0),2)
-                        #    cv2.drawMarker(frame, (cx,cy), (0,0,255), markerType=cv2.MARKER_CROSS, markerSize=10, thickness=1)
-                        break
+                        if white_ratio < 90 and white_ratio > 80:
+                            frame = cv2.ellipse(frame,ellipse,(0,0,255),2)
+                            cv2.rectangle(frame, (xmin_c, ymin_c), (xmax_c, ymax_c), (0, 255, 0), 2)
+                        #cv2.drawMarker(frame, (cx,cy), (0,0,255), markerType=cv2.MARKER_CROSS, markerSize=10, thickness=1)
+
+    frame_trimming = frame[ymin:ymax, xmin:xmax]
+    frame_trimming2 = frame2[ymin:ymax, xmin:xmax]                    
     
+    #測定範囲(青枠)
+    #cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
 
-    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
     cv2.imshow('output', frame)
+    cv2.imshow('output2', frame_trimming2)
+    cv2.imshow('edges', edges)
+    cv2.imshow('reverse', img_reverse)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    video.write(frame_trimming)
+    video2.write(frame_trimming2)
+    video_bin.write(cv2.cvtColor(img_thresh, cv2.COLOR_GRAY2BGR)) #2値化映像保存
+    video_edges.write(cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)) 
+    video_reverse.write(cv2.cvtColor(img_reverse, cv2.COLOR_GRAY2BGR)) 
+
+    c = time.time()
+    if c - t >= 15 or cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
