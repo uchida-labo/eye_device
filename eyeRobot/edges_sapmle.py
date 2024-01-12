03a5104191a7b04bb3a6fb9b577e4cb683f9b07e
nakano-kota <naknao.kota.yb@tut.jp>
2024-01-12 16:37:54 +0900

    delete edges_samole.py

--- eyeRobot/edges_sapmle.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread('/Users/nakanokota/Desktop/fig2.jpg', cv2.IMREAD_UNCHANGED)
-rev = cv2.bitwise_not(img)
-edges = cv2.Canny(rev, 0, 130)
-
-cv2.imwrite('/Users/nakanokota/Desktop/edges2.jpg', edges)
